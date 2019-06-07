4333c5ad073d77e24187d48451603db6e21cbe9b
Inho Oh <inho.oh@sk.com>
2019-06-07 10:07:27 +0900

    apply debian patches
    no-werror.diff
    unvendor-zlib.diff
    fix-libgrpc++-soname.diff
    add-wrap-memcpy-flags.diff
    install_libdir.patch
    no-embed-libs.patch
    remove-missing-files-in-gemspec.patch
    no-make-in-extconf.patch
    use-system-grpc.patch
    fix-protoc-path.patch
    add_grpc_libdir.patch
    libgrpcpp_channelz_symlink.patch
    
    Referenced the v1.16.1-1 packaging script in the Ubuntu.
    https://launchpad.net/ubuntu/+source/grpc

--- src/ruby/spec/spec_helper.rb
+++ src/ruby/spec/spec_helper.rb
@@ -17,7 +17,6 @@ root_dir = File.expand_path(File.join(spec_dir, '..'))
 lib_dir = File.expand_path(File.join(root_dir, 'lib'))
 
 $LOAD_PATH.unshift(spec_dir)
-$LOAD_PATH.unshift(lib_dir)
 $LOAD_PATH.uniq!
 
 # set up coverage
